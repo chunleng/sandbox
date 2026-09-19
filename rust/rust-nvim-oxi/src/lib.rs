0d0a88b9ec35d792deb2adcdf9133de99843a8dc
Lim Chun Leng <limchunleng@gmail.com>
2026-09-19 11:28:12 +0900

    refactor(rust): put calc in lua calc module for `nvim-oxi` example

--- rust/rust-nvim-oxi/src/lib.rs
+++ rust/rust-nvim-oxi/src/lib.rs
@@ -1,19 +1,10 @@
-use nvim_oxi::{Dictionary, Function, Object, print};
+mod calc;
+use nvim_oxi::{Dictionary, Object, print};
 
 #[nvim_oxi::plugin]
 fn sample() -> Dictionary {
     print!("sample function called!");
-    let add = Function::from_fn(|(a, b): (i32, i32)| a + b);
-
-    let multiply = Function::from_fn(|(a, b): (i32, i32)| a * b);
-
-    let compute = Function::from_fn(|(fun, a, b): (Function<(i32, i32), i32>, i32, i32)| {
-        fun.call((a, b)).unwrap()
-    });
-
-    Dictionary::from_iter([
-        ("add", Object::from(add)),
-        ("multiply", Object::from(multiply)),
-        ("compute", Object::from(compute)),
-    ])
+    Dictionary::from_iter([("calc", Object::from(calc::module()))])
 }
+
+
